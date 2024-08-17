ad8308607cdff18706dc9976897ed88c3211dc91
Kooreis <kooreis.official@gmail.com>
2024-08-17 05:21:58 -0500

    In this update, the user is prompted to enter the elements of the array. A for loop is used to iterate over the array and assign the user's input to each element.

--- solution.java
+++ solution.java
@@ -1,10 +1,4 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the length of the array:");
-        int n = scanner.nextInt();
-        int[] arr = new int[n];
-    }
-}
+System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
